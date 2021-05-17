29c3325fe870acc154592e536d3e42383aaea479
Anna Nazaryan <anna.nazaryn@iternal.group>
2021-05-17 19:10:25 +0300

    group room content and scale

--- src/App.js
+++ src/App.js
@@ -5,29 +5,43 @@ import '@svgdotjs/svg.draggable.js'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
     this.defaults = {
-      roomSize: {
+      containerSize: {
         width: 500,
         height: 500,
       },
-      doorWidth: 70
+      settings: {
+        roomSize: {
+          width: 5000,
+          height: 5000,
+        },
+        doorWidth: 700,
+      }
     };
+    this.state = {};
   }
 
   componentDidMount() {
-    this.draw = SVG().addTo('#container').size(`100%`, `100%`);
-    const draw = this.draw;
+    this.draw = SVG()
+        .addTo(this.container)
+        .size(this.container.clientWidth, this.container.clientHeight)
+        .on('drop', this.dropImg);
 
-    const {roomSize, doorWidth} = this.defaults;
+    const draw = this.draw;
+    const {roomSize, doorWidth} = this.defaults.settings;
+
+    this.roomSize = {
+      width: draw.text(roomSize.width.toString()).move(50, 510),
+      height: draw.text(roomSize.height.toString()).move(510, 50)
+    }
 
     this.room = draw
       .rect(roomSize.width, roomSize.height)
       .fill(`#F7F5F4`);
 
-
-    this.door = draw.line(0, 0, doorWidth, 0).stroke({ color: '#f06', width: 10 });
+    this.door = draw
+        .line(0, 0, doorWidth, 0)
+        .stroke({ color: '#f06', width: 100 });
     this.door.draggable().on('dragmove.namespace', e => {
       e.preventDefault();
       const { handler, box } = e.detail;
@@ -36,29 +50,45 @@ class App extends React.Component {
         y = 0
       }
       handler.move(x, y)
-    })
+    });
 
+    var lamp = draw.image('lamp.png')
+        .size(2000, 2000)
+        .move(150, 150)
 
+    this.roomContent = draw.group();
+    this.roomContent.add(this.room);
+    this.roomContent.add(this.door);
+    this.roomContent.add(lamp);
+    this.recalculateScale();
+  }
+
+  recalculateScale = () => {
+    const scaleWidth = this.defaults.containerSize.width/this.room.width();
+    const scaleHeight = this.defaults.containerSize.height/this.room.height();
+    const scale = Math.min(scaleWidth, scaleHeight);
+    this.roomContent.transform({scale, origin: {x: 0, y: 0}});
   }
 
   changeRoomWidth = e => {
     e.preventDefault();
-    const width = e.target.value;
-
+    const width = parseInt(e.target.value);
+    this.roomSize.width.text(width.toString());
     this.room.width(width);
+    this.recalculateScale();
   }
   changeRoomHeight = e => {
     e.preventDefault();
     const height = e.target.value;
-
+    this.roomSize.height.text(height.toString());
     this.room.height(height);
+    this.recalculateScale();
   }
 
   changeDoorSize = e => {
     e.preventDefault();
     const width = e.target.value;
     this.door.width(width);
-    //this.door.plot(0, 0, width, 0);
   }
 
   allowDrop = e => {
@@ -70,30 +100,37 @@ class App extends React.Component {
   dropImg = (e, _) => {
     e.preventDefault();
     var image = this.draw.image(e.dataTransfer.getData("imgSrc"))
-    image.size(200, 200).move(150, 150).draggable();
+    image
+        .size(200, 200)
+        .move(150, 150)
+        .draggable();
   }
 
   render() {
-    const {roomSize, doorWidth} = this.defaults;
+    const {roomSize, doorWidth} = this.defaults.settings;
     return (
         <div style={{display: `grid`, gridTemplateColumns: `1fr 1fr`}}>
           <div>
             <h1>settings</h1>
             <div>
               room size
-              <input onChange={this.changeRoomWidth} type={`number`} defaultValue={roomSize.width}/>
-              <input onChange={this.changeRoomHeight} type={`number`} defaultValue={roomSize.height}/>
+              <input onChange={this.changeRoomWidth} type={`number`} step={1000} defaultValue={roomSize.width}/>
+              <input onChange={this.changeRoomHeight} type={`number`} step={1000} defaultValue={roomSize.height}/>
             </div>
             <div>
               door width
-              <input onChange={this.changeDoorSize} type={`number`} defaultValue={doorWidth}/>
+              <input onChange={this.changeDoorSize} type={`number`} step={100} defaultValue={doorWidth}/>
             </div>
             <div>
               image to drag:
               <img src={`sofa.png`}  draggable="true" onDragStart={this.dragImg}/>
             </div>
           </div>
-          <div id={`container`} style={{height: 500, width: 500}} onDragOver={this.allowDrop}  onDrop={this.dropImg}>
+          <div id={`container`}
+               ref={(container) => {this.container = container}}
+               style={{height: `100vh`, width: `100%`}}
+               onDragOver={this.allowDrop}
+          >
           </div>
         </div>
 
